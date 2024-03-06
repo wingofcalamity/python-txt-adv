d34652189df50a4517a3d95f36ee67ae1ed78a9b
Vince <wingofcalamity@gmail.com>
2024-03-06 20:27:24 +0100

    random names for enemis

---
+++ data/Names.py
@@ -0,0 +1,15 @@
+names = [
+    "Glorb",
+    "FooBar",
+    "placeholder"
+    "Amongus",
+    "Alex",
+    "Wii U Browser",
+    "& Knuckles",
+    "Skibidi Toilet",
+    "Herta",
+    "Texas Instruments TI-30 ECO RS"
+    "Google Stadia",
+    "Flipflop",
+    "PeePeePooPoo"
+]
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+from random import randint
+
 from models.Player import Player
 from models.Enemy import Enemy
 from interactions.Encounter import encounter
+from data.Names import names
+
+
+def create_enemy():
+    enemy_name = names[randint(0, len(names) - 1)]
+    enemy_hp = randint(3, 15)
+    enemy_damage = randint(1, 5)
+    return Enemy(enemy_name, enemy_hp, enemy_damage)
 
 
 def main():
     me = Player()
     while True:
-        glorb = Enemy("Glorb", 15, 3)
-        encounter(me, glorb)
+        enemy = create_enemy()
+        encounter(me, enemy)
 
 
 if __name__ == "__main__":
